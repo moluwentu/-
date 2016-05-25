42e8983ab429564e10bc52f7427a79f18b880fa4
叶慧伟 <yehuiwei@yehuiweidemacbook-pro.local>
2016-05-25 14:29:30 +0800

    Initial Commit

---
+++ travel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  travel
+//
+//  Created by 叶慧伟 on 16/5/25.
+//  Copyright © 2016年 叶慧伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ travel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  travel
+//
+//  Created by 叶慧伟 on 16/5/25.
+//  Copyright © 2016年 叶慧伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
